a78168a164c1c157f8bdab46bdc8fffff051f5a8
Simon Wotherspoon <Simon.Wotherspoon@utas.edu.au>
2013-09-07 10:50:16 +1000

    Changed median calculation

--- R/SGAT.R
+++ R/SGAT.R
@@ -1395,7 +1395,7 @@ curve.model <- function(datetime,light,segments,
   ## Convert to solar time.
   sun <- solar(datetime)
   ## Median time in each segment
-  tm <- as.vector(tapply(datetime, segments, median) ) + ISOdatetime(1970, 1, 1, 0, 0, 0, tz = "GMT")
+  tm <- .POSIXct(sapply(split(datetime,segments),median),"GMT")
   ## Fixed x locations
   fixedx <- rep(fixedx,length=nrow(x0))
   ## Times (hours) between observations
