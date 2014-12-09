175c4dd81b00d0a6c38373afd1039be7eda4266f
Simon Wotherspoon <Simon.Wotherspoon@utas.edu.au>
2014-12-10 07:54:21 +1100

    First working prototype

--- R/Lattice.R
+++ R/Lattice.R
@@ -1,14 +1,20 @@
-cellFromLonLat <- function(raster,ps) {
-  if(!(is.na(projection(raster)) || isLonLat(raster))) {
-    ps <- SpatialPoints(ps,proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
-    ps <- coordinates(spTransform(ps,projection(raster,FALSE)))
-  }
-  cellFromXY(raster,ps)
+##' Calculate great circle distancces between points.
+##'
+##' Compute the great circle distances from x1 to x2.
+##' @title Great Circle Distance
+##' @param x1 a two column matrix of (lon,lat) locations.
+##' @param x2 a two column matrix of (lon,lat) locations.
+##' @return vector of interpoint distances (km)
+##' @export
+gcDist <- function(x1,x2) {
+  rad <- pi/180
+  s1 <- sin(rad*x1[,2L])
+  s2 <- sin(rad*x2[,2L])
+  6378.137*acos(pmin.int(sqrt(1-s1*s1)*sqrt(1-s2*s2)*cos(rad*(x2[,1L]-x1[,1L]))+s1*s2,1))
 }
 
-
-
-gcDist <- function(x1,x2) {
+## Alternate code
+gcDist0 <- function(x1,x2) {
   rad <- pi/180
   6378.137*acos(pmin.int(
     cos(rad*x1[,2L])*cos(rad*x2[,2L])*cos(rad*(x2[,1L]-x1[,1L]))+
@@ -16,7 +22,101 @@ gcDist <- function(x1,x2) {
 }
 
 
+##' Calculate great circle distancces between all pairs of points.
+##'
+##' Compute the great circle distances from every point in x1 to every
+##' point is x2, and return the result as a matrix.
+##' @title Great Circle Distance
+##' @param x1 a two column matrix of (lon,lat) locations.
+##' @param x2 a two column matrix of (lon,lat) locations.
+##' @return vector of interpoint distances (km)
+##' @export
+gcOuterDist <-function(x1,x2) {
+  rad <- pi/180
+  s1 <- sin(rad*x1[,2L])
+  s2 <- sin(rad*x2[,2L])
+  matrix(6378.137*acos(pmin.int(
+    (sqrt(1-s1*s1)%o%sqrt(1-s2*s2))*cos(outer(rad*x1[,1L],rad*x2[,1L],"-"))+s1%o%s2,1)),
+         length(x1),length(x2))
+}
 
+
+
+
+
+
+
+##' Threshold Model Structures for Essie
+##'
+##' Essie requires a model structure that describes the model being
+##' fitted. These functions generate basic model structures for
+##' threshold twilight data that should provide a suitable starting
+##' point for most analyses.
+##'
+##' The \code{essie.threshold.model} function constructs a model structure
+##' assuming that each twilight time is associated with a single
+##' location.
+##'
+##' One of several models models may be selected for the errors in
+##' twilight times.  The errors in twilight time are defined as the
+##' difference in the observed and true times of twilight, with sign
+##' selected so that a positive error always corresponds to a sunrise
+##' observed after the true time of sunrise, and sunset observed
+##' before the true time of sunset. That is, a positive error
+##' corresponds to the observed light level being lower than expected.
+##'
+##' The properties of the twilight model are determined by
+##' \code{alpha}, which must be either a vector of parameters that are
+##' to be applied to each twilight.
+##'
+##' The \code{twilight.model} argument selects the distribution of the
+##' twilight errors
+##' \describe{
+##' \item{'Normal'}{Normally distributed with mean \code{alpha[,1]} and
+##' standard deviation \code{alpha[,2]},}
+##' \item{'LogNormal'}{Log Normally distributed so the log errors have
+##' mean \code{alpha[,1]} and standard deviation \code{alpha[,2]}, or}
+##' \item{'Gamma'}{Gamma distributed with shape \code{alpha[,1]} and
+##' rate \code{alpha[2]}.}
+##' }
+##'
+##' The initialization locations \code{x0} are only required when
+##' specifying fixed locations.
+##'
+##' Essie assumes that the average speed of travel between successive
+##' locations is Gamma distributed. By default, the speed of travel is
+##' calculated based on the time intervals between the twilights (in
+##' hours), but the intervals of time actually available for travel
+##' can be specified directly with the \code{dt} argument. The
+##' parameters \code{beta[1]} and \code{beta[2]} specify the shape and
+##' rate of the Gamma distribution of speeds.
+##'
+##' At this point Essie cannot deal with missing values.
+##'
+##' @title Threshold Model Structures (Essie)
+##' @param twilight the observed times of twilight as POSIXct.
+##' @param rise logical vector indicating which twilights are sunrise.
+##' @param twilight.model the model for the errors in twilight times.
+##' @param alpha parameters of the twilight model.
+##' @param beta parameters of the behavioural model.
+##' @param logp0 function to evaluate any additional contribution to
+##' the log posterior from the twilight locations.
+##' @param x0 suggested starting points for twilight locations.
+##' @param fixed logical vector indicating which twilight locations
+##' to hold fixed.
+##' @param dt time intervals for speed calculation in hours.
+##' @param zenith the solar zenith angle that defines twilight.
+##' @return a list with components
+##' \item{\code{logpk}}{function to evaluate the contributions to the
+##' log posterior from the k-th twilight}
+##' \item{\code{logpbk}}{function to evaluate contribution to
+##' the log posterior from the behavioural model for the k-th track segment.}
+##' \item{\code{fixed}}{a logical vector indicating which locations
+##' should remain fixed.}
+##' \item{\code{x0}}{an array of initial twilight locations.}
+##' \item{\code{time}}{the twilight times.}
+##' \item{\code{rise}}{the sunrise indicators.}
+##' @export
 essie.threshold.model <- function(twilight,rise,
                                   twilight.model=c("LogNormal","Gamma","Normal"),
                                   alpha,beta,
@@ -60,28 +160,56 @@ essie.threshold.model <- function(twilight,rise,
   }
 
   list(
-    time=twilight,
-    rise=rise,
-    n=length(twilight),
-    alpha=alpha,
-    beta=beta,
     logpk=logpk,
     logbk=logbk,
+    fixed=fixed,
     x0=x0,
-    fixed=fixed)
+    time=twilight,
+    rise=rise,
+    alpha=alpha,
+    beta=beta)
 }
 
 
-
-essie <- function(model,grid,epsilon=1.0E-2,verbose=interactive()) {
-
+##' Fit an Essie model
+##'
+##' Essie is a discrete analog of Stella that only considers locations
+##' on a lattice of grid points.
+##'
+##' The posterior probability that the tag is at a particular location
+##' is determined by a two pass recursive algorithm.  The forward
+##' sweep propogates location information forward in time, the
+##' backward sweep propogates location information backward in time,
+##' and the full posterior is a compromise of these two.
+##'
+##' The method is only approximate and is controlled by the two
+##' tolerances \code{epsilon1} and \code{epsilon2}.  The first limits
+##' the precision of the likelihood - locations for which the
+##' likelihood fall below \code{epsilon1} of its maximum are ignored.
+##' The second limits the precision of the transition probabilities -
+##' locations for which the probability below \code{epsilon2} of the
+##' maximum are ignored in the movement calculation.  The smaller
+##' these parameters are set, the slower but more accurate the fit.
+##'
+##' @title Essie
+##' @param model a model structure generated by \code{essie.threshold.model}.
+##' @param grid a raster object defining the grid
+##' @param epsilon1 likelihood tolerance
+##' @param epsilon2 transition probability tolerance.
+##' @param verbose report progress at prompt?
+##' @return a list with elements
+##' \item{\code{grid}}{a raster object that defines the grid.}
+##' \item{\code{times}}{the times corresponding to the location estimates.}
+##' \item{\code{lattice}}{a sparse grid representation of the posterior location probabilities.}
+##' @export
+essie <- function(model,grid,epsilon1=1.0E-3,epsilon2=1.0E-8,verbose=interactive()) {
 
   normalize <- function(x) {
     s <- sum(x)
-    if(s < 1.0E-12) rep(0,length(x)) else x/sum(x)
+    if(s < 1.0E-16) rep(0,length(x)) else x/sum(x)
   }
 
-  n <- model$n
+  n <- length(model$time)
   pts <- lonlatFromCell(grid,1:ncell(grid))
   fixed <- integer(n)
   fixed[model$fixed] <- cellFromLonLat(grid,model$x0[model$fixed,])
@@ -93,7 +221,7 @@ essie <- function(model,grid,epsilon=1.0E-2,verbose=interactive()) {
       list(cs=fixed[k],ps=1)
     } else {
       logps <- model$logpk(k,pts[cs,,drop=FALSE])
-      keep <- logps > log(epsilon)+max(logps)
+      keep <- logps > log(epsilon1)+max(logps)
       list(cs=cs[keep],ps=normalize(exp(logps[keep])))
     }
   })
@@ -111,12 +239,11 @@ essie <- function(model,grid,epsilon=1.0E-2,verbose=interactive()) {
       cat(sprintf("%6d",k));
       flush.console()
     }
-
     xs0 <- xs
     xs <- pts[lattice[[k]]$cs,,drop=F]
     as0 <- as
     as <- 0
-    for(i in which(as0>0))
+    for(i in which(as0 > epsilon2*max(as0)))
       as <- as + as0[i]*exp(model$logbk(k-1,xs0[i,,drop=F],xs))
     as <- normalize(as*lattice[[k]]$ps)
     lattice[[k]]$as <- as
@@ -136,12 +263,11 @@ essie <- function(model,grid,epsilon=1.0E-2,verbose=interactive()) {
       cat(sprintf("%6d",k));
       flush.console()
     }
-
     xs0 <- xs
     xs <- pts[lattice[[k]]$cs,,drop=F]
     bs0 <- bs
     bs <- 0
-    for(i in which(bs0>0))
+    for(i in which(bs0 > epsilon2*max(bs0)))
       bs <- bs + bs0[i]*exp(model$logbk(k,xs,xs0[i,,drop=F]))
     bs <- normalize(bs*lattice[[k]]$ps)
     lattice[[k]]$bs <- bs
@@ -151,12 +277,25 @@ essie <- function(model,grid,epsilon=1.0E-2,verbose=interactive()) {
 }
 
 
-lattice.raster <- function(obj,k,type=c("posterior","forward","backward","likelihood")) {
+
+##' Return the probabilities from an Essie model as a raster object.
+##'
+##' This function returns the likelihood or the posterior probability
+##' of a single location as a raster object.
+##'
+##' @title Essie probability rasters
+##' @param obj a fitted object generated by \code{essie}
+##' @param k the index of the location
+##' @param type whether to return the full estimate, the estimate from
+##' the forward or backward pass of the algorithm or the likelihood.
+##' @return a raster of probabilities/likelihoods
+##' @export
+essie.raster <- function(obj,k,type=c("full","forward","backward","likelihood")) {
   g <- raster(obj$grid)
   l <- obj$lattice[[k]]
   type <- match.arg(type)
   g[l$cs] <- switch(type,
-                    posterior=l$as*l$bs/l$ps,
+                    full=l$as*l$bs/l$ps,
                     forward=l$as,
                     backward=l$bs,
                     likelihood=l$ps)
@@ -164,7 +303,20 @@ lattice.raster <- function(obj,k,type=c("posterior","forward","backward","likeli
 }
 
 
-lattice.mean <- function(obj,type=c("posterior","forward","backward")) {
+
+##' Compute posterior mean or mode locations from an Essie fit
+##'
+##' These functions compute the posterior mean or posterior mode
+##' locations for each time point from and Essie fit.
+##' @title Essie locations
+##' @param obj a fitted object generated by \code{essie}
+##' @param type whether to return the full estimate or just the
+##' estimate from the forward or backward pass of the algorithm.
+##' @return a list with elements
+##' \item{\code{times}}{the times corresponding to the location estimates.}
+##' \item{\code{x}}{a two column matrix of positions.}
+##' @export
+essie.mean <- function(obj,type=c("full","forward","backward")) {
   type <- match.arg(type)
   cs <- unlist(lapply(obj$lattice,function(l) {
     ps <- switch(type,
@@ -173,11 +325,13 @@ lattice.mean <- function(obj,type=c("posterior","forward","backward")) {
                  backward=l$bs)
     l$cs[which.max(ps)]
   }))
-  lonlatFromCell(obj$grid,cs)
+  x <- lonlatFromCell(obj$grid,cs)
+  list(time=obj$time,x=x)
 }
 
-
-lattice.maxp <- function(obj,type=c("posterior","forward","backward")) {
+##' @rdname essie.mean
+##' @export
+essie.mode <- function(obj,type=c("full","forward","backward")) {
   type <- match.arg(type)
   pts <- lonlatFromCell(obj$grid,1:ncell(obj$grid))
   do.call(rbind,lapply(obj$lattice,function(l) {
@@ -188,4 +342,3 @@ lattice.maxp <- function(obj,type=c("posterior","forward","backward")) {
     colSums(ps*pts[l$cs,,drop=F])/sum(ps)
   }))
 }
-
